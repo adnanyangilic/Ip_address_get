ca9192cfdf872a0b889eb53d282f0be4a3cc2929
adnanyangilic <adnan.alistika@gmail.com>
2022-09-25 19:32:19 +0300

    Proje dosyası ekle.

---
+++ Ipaddress/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Ipaddress.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ipaddress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHttpContextAccessor _accessor;
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string ipAddress = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewBag.IPAddress = ipAddress;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
